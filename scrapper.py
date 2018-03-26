697bce4eae132eb31c3fc0637cefd0acd5b149e3
yash upadhyay <yashup1997@gmail.com>
2018-03-26 22:43:15 +0530

    script for downloading all the images on a webpage

---
+++ scrapper.py
@@ -0,0 +1,30 @@
+#importing required libraries
+import urllib.request as jk
+from bs4  import BeautifulSoup
+from os.path  import basename
+import requests
+import os
+
+#setting the page from where you want to download images
+websiteCode = jk.urlopen("http://www.imfdb.org/wiki/Category:Assault_Rifle").read()
+soup = BeautifulSoup(websiteCode,"lxml")
+
+images = []
+images = soup.findAll('img')
+
+link = []
+
+# generating list of url of he images
+for image in images:
+    link.append(image.get('src'))
+
+str1 = 'http://www.imfdb.org'
+link_final = []  
+for l in link:
+    link_final.append(str1+l)
+
+for link in link_final:
+        with open(basename(link), "wb") as f:
+            f.write(requests.get(link).content)
+
+
